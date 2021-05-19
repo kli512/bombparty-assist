6dc4344961aba3f4a0aed70fe160c34c7f9efead
Kevin Li <li.kevin512@gmail.com>
2021-05-19 02:54:08 -0500

    Only show answer box when in game, filling README

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# BombParty Assist
+
+This is a python program (using selenium) to "assist" players in BombParty at jklm.fun
+
+Uses selenium to monitor the webpage for changes in the "syllable", then generates a list of all words that contain that syllable (according to the dictionary I believe is used by bombparty). It then injects the words below the chat window in bombparty cleanly, additionally allowing sorting of the generated solutions.
+
+## bombparty dictionary
+
+dictionary information source: https://www.reddit.com/r/BombParty/comments/3lehxq/a_nearly_exhaustive_subset_of_the_bombparty/
+
 dict is created by running `cat enable1.txt sowpods.txt word_list_moby_crossword.flat.txt | sort | uniq >dict.txt`
 
 http://norvig.com/ngrams/sowpods.txt
--- insert_words.js
+++ insert_words.js
@@ -1,69 +1,72 @@
-// could put sort in the python and just have it insert all three lines for words
-var ascendingWords = smartWords.slice().sort((a, b) => a.length - b.length);
-var descendingWords = smartWords.slice().sort((a, b) => b.length - a.length);
-
 var injections = document.getElementsByClassName('injection');
 
 for (var i = injections.length - 1; i >= 0; --i) {
   injections[i].remove();
 }
 
-function generateWordBankDiv(words, id, hidden=true) {
-  wordBankDiv = document.createElement('div');
-  wordBankDiv.hidden = hidden;
-  wordBankDiv.id = id;
-  wordBankDiv.className = 'chat pane injection'
+if (smartWords.length > 0) {
+  // could put sort in the python and just have it insert all three lines for words
+  var ascendingWords = smartWords.slice().sort((a, b) => a.length - b.length);
+  var descendingWords = smartWords.slice().sort((a, b) => b.length - a.length);
 
-  innerChat = document.createElement('div');
-  innerChat.className = 'log darkScrollbar';
 
-  words.forEach(e => {
-    chatMessage = document.createElement('div');
-    chatMessage.className = 'system';
+  function generateWordBankDiv(words, id, hidden=true) {
+    wordBankDiv = document.createElement('div');
+    wordBankDiv.hidden = hidden;
+    wordBankDiv.id = id;
+    wordBankDiv.className = 'chat pane injection'
 
-    line = document.createElement('span');
-    line.className = 'text';
-    line.innerText = e;
+    innerChat = document.createElement('div');
+    innerChat.className = 'log darkScrollbar';
 
-    chatMessage.appendChild(line);
-    innerChat.appendChild(chatMessage);
+    words.forEach(e => {
+      chatMessage = document.createElement('div');
+      chatMessage.className = 'system';
 
-  });
+      line = document.createElement('span');
+      line.className = 'text';
+      line.innerText = e;
 
-  wordBankDiv.appendChild(innerChat);
+      chatMessage.appendChild(line);
+      innerChat.appendChild(chatMessage);
 
-  return wordBankDiv
-}
+    });
 
-// Generate control bar
-function generateButtonCode(showId, allIds) {
-  let str = 'javascript:';
-  for (const id of allIds) {
-    str += 'document.getElementById("' + id + '").hidden = true;';
+    wordBankDiv.appendChild(innerChat);
+
+    return wordBankDiv
   }
-  str += 'document.getElementById("' + showId + '").hidden = false;';
 
-  return str;
+  // Generate control bar
+  function generateButtonCode(showId, allIds) {
+    let str = 'javascript:';
+    for (const id of allIds) {
+      str += 'document.getElementById("' + id + '").hidden = true;';
+    }
+    str += 'document.getElementById("' + showId + '").hidden = false;';
+
+    return str;
+  }
+
+  controlBar = document.createElement('div');
+  controlBar.className = 'tabs injection';
+  banks = ['SmartBank', 'AscendingBank', 'DescendingBank'];
+  symbols = ['🧠', '⬆️', '⬇️']
+
+  for (var i = 0; i < banks.length; i++) {
+    button = document.createElement('a');
+    button.title = banks[i].slice(0, -4) + ' sort';
+    button.innerText = symbols[i];
+    button.href = generateButtonCode(banks[i], banks);
+    controlBar.appendChild(button);
+  }
+
+  // Append control bar and word banks to sidebar
+  sidebarElem = document.getElementsByClassName('sidebar')[0];
+
+  sidebarElem.appendChild(controlBar);
+
+  sidebarElem.appendChild(generateWordBankDiv(smartWords, 'SmartBank', false));
+  sidebarElem.appendChild(generateWordBankDiv(ascendingWords, 'AscendingBank'));
+  sidebarElem.appendChild(generateWordBankDiv(descendingWords, 'DescendingBank'));
 }
-
-controlBar = document.createElement('div');
-controlBar.className = 'tabs injection';
-banks = ['SmartBank', 'AscendingBank', 'DescendingBank'];
-symbols = ['🧠', '⬆️', '⬇️']
-
-for (var i = 0; i < banks.length; i++) {
-  button = document.createElement('a');
-  button.title = banks[i].slice(0, -4) + ' sort';
-  button.innerText = symbols[i];
-  button.href = generateButtonCode(banks[i], banks);
-  controlBar.appendChild(button);
-}
-
-// Append control bar and word banks to sidebar
-sidebarElem = document.getElementsByClassName('sidebar')[0];
-
-sidebarElem.appendChild(controlBar);
-
-sidebarElem.appendChild(generateWordBankDiv(smartWords, 'SmartBank', false));
-sidebarElem.appendChild(generateWordBankDiv(ascendingWords, 'AscendingBank'));
-sidebarElem.appendChild(generateWordBankDiv(descendingWords, 'DescendingBank'));
